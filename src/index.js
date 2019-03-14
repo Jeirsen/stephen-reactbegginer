e46ce3d3899222937a150256e51c2005ec26a8cb
Jeirsen <jeirsen@gmail.com>
2019-03-13 23:01:38 -0500

    added new properties

--- src/index.js
+++ src/index.js
@@ -5,10 +5,26 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
-      <CommentDetail author="Tom" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 2:00PM"
+        content="Nice blog post"
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 4:45PM"
+        content="Nice course"
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 5:00PM"
+        content="Nice App"
+      />
+      <CommentDetail
+        author="Tom"
+        timeAgo="Today at 10:00AM"
+        content="Check that service"
+      />
     </div>
   );
 };
